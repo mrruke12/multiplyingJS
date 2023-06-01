99b60cdfd42335a46e3d82937a5c4429e3469e07
mrruke12 <135268579+mrruke12@users.noreply.github.com>
2023-06-02 03:05:25 +0600

    Update code.js

--- code.js
+++ code.js
@@ -12,36 +12,47 @@ function fill(tr, str, cls = '') { // функция для заполнения
 document.addEventListener('DOMContentLoaded', () => {
     const table = document.querySelector('#table')
 
-    let a = '64595654334' // множимое
-    let b = '12312321312'  // множитель
+    let a = '1' // множимое
+    let b = '1'  // множитель
 
-    if (eval(`${b}>${a}`)) [a,b] = [b,a] // в переменной "а" всегда большее число
+    function main() {
+        table.innerHTML = '' // очищаем таблицу
+        if (eval(`${b}>${a}`)) [a,b] = [b,a] // в переменной "а" всегда большее число
 
-    const edge =  b.length + (a * b[0]).toString().length // итоговое количество ячеек в строке
-    let indent = edge // текущий отступ
+        const edge =  b.length + (a * b[0]).toString().length // итоговое количество ячеек в строке
+        let indent = edge // текущий отступ
 
-    let task1 = table.appendChild(document.createElement('tr'))
-    doIndent(task1, indent - a.length - 1)
-    fill(task1, a) // выводим множимое
+        let task1 = table.appendChild(document.createElement('tr'))
+        doIndent(task1, indent - a.length - 1)
+        fill(task1, a) // выводим множимое
 
-    let task2 = table.appendChild(document.createElement('tr'))
-    doIndent(task2, indent - b.length - 1, 'bottom')
-    fill(task2, b, 'bottom') // выводим множитель
+        let task2 = table.appendChild(document.createElement('tr'))
+        doIndent(task2, indent - b.length - 1, 'bottom')
+        fill(task2, b, 'bottom') // выводим множитель
 
-    for (let i = b.length-1; i >= 0; i--) { // поочередные операции умножения множимого на числа множителя
-        let tr = table.appendChild(document.createElement('tr'))
-        const num = eval(`${b[i]} * ${a}`).toString() // текущее число, которое будет выведено
-        
-        doIndent(tr, indent - num.length - 1, i == 0 ? 'bottom' : '') // добавляем пустые ячейки до
-        fill(tr, num, i == 0 ? 'bottom' : '') // выводим промежуточное число
-        doIndent(tr, edge - indent, i == 0 ? 'bottom' : '') // добавляем пустые ячейки после
+        for (let i = b.length-1; i >= 0; i--) { // поочередные операции умножения множимого на числа множителя
+            let tr = table.appendChild(document.createElement('tr'))
+            const num = eval(`${b[i]} * ${a}`).toString() // текущее число, которое будет выведено
+            
+            doIndent(tr, indent - num.length - 1, i == 0 ? 'bottom' : '') // добавляем пустые ячейки до
+            fill(tr, num, i == 0 ? 'bottom' : '') // выводим промежуточное число
+            doIndent(tr, edge - indent, i == 0 ? 'bottom' : '') // добавляем пустые ячейки после
 
-        indent--
+            indent--
+        }
+
+        // выводим ответ
+        let answer = table.appendChild(document.createElement('tr'))
+        const num = eval(`${a * b}`).toString() // текущее число, которое будет выведено
+        num.split('').forEach(c => { answer.appendChild(document.createElement('td')).innerHTML = c })
+        console.log(num) // выводим ответ в консоль
     }
 
-    // выводим ответ
-    let answer = table.appendChild(document.createElement('tr'))
-    const num = eval(`${a * b}`).toString() // текущее число, которое будет выведено
-    num.split('').forEach(c => { answer.appendChild(document.createElement('td')).innerHTML = c })
-    console.log(num) // выводим ответ в консоль
+    document.querySelector('#btn').addEventListener("click", () => { // отлавливаем ввод новых чисел
+        a = document.querySelector('#a').value
+        b = document.querySelector('#b').value
+        main()
+    })
+
+    main()
 })
